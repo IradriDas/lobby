ae204411a884f5934146f27a3a313847e1e7832f
IradriDas <slicingexorcism@gmail.com>
2026-07-24 10:37:59 +0530

    changed a color

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -17,7 +17,7 @@ const Sidebar = () => {
           const isActive = (pathName === link.route);
 
           return (
-            <Link href={link.route} key={link.label} className={cn("flex gap-4 items-center p-4 rounded-lg justify-start", { "bg-[#0E78F9]": isActive })}>
+            <Link href={link.route} key={link.label} className={cn("flex gap-4 items-center p-4 rounded-lg justify-start", { "bg-[#193abc]": isActive })}>
               <Image src={link.imgUrl} alt={link.label} width={24} height={24} />
               <p className="text-lg font-semibold max-lg:hidden">{link.label}</p>
             </Link>
