44c9cbaa909f5b4b921f9fc5be3a12e78c10612c
IradriDas <slicingexorcism@gmail.com>
2026-07-25 02:31:48 +0530

    Un-used components deleted

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -4,10 +4,6 @@ import { cn } from "@/lib/utils";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "./ui/button";
 
