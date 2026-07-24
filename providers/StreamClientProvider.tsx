a0f708431610ffbc3528ede04f60f0f238c36820
IradriDas <slicingexorcism@gmail.com>
2026-07-24 10:42:07 +0530

    basically its a bridge between the app and Stream's video servers

---
+++ providers/StreamClientProvider.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import {
+  StreamVideo,
+  StreamVideoClient,
+  User,
+} from "@stream-io/video-react-sdk";
+import { useUser } from "@clerk/nextjs";
+import { tokenProvider } from "@/actions/stream.action";
+import Loader from "@/components/Loader";
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+
+export const StreamVideoProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [videoClient, setVideoClient] = useState<StreamVideoClient | undefined>(
+    undefined,
+  );
+  const { user, isLoaded } = useUser();
+
+  useEffect(() => {
+    if (!isLoaded || !user) return;
+    if (!apiKey) throw new Error("Stream API key is not defined");
+
+    const streamUser: User = {
+      id: user.id,
+      name: user.username || user.id,
+      image: user.imageUrl,
+    };
+
+    const client = new StreamVideoClient({
+      apiKey,
+      user: streamUser,
+      tokenProvider,
+    });
+
+    setVideoClient(client);
+
+    return () => {
+      client.disconnectUser().catch(console.error);
+      setVideoClient(undefined);
+    };
+  }, [user, isLoaded]);
+
+  if (!videoClient) return <Loader />;
+
+  return (
+    <>
+      <StreamVideo client={videoClient}>{children}</StreamVideo>
+    </>
+  );
+};
